a251196854b2fb29b71c720678b3536fe246063f
Nicos Panayides <nicosp@gmail.com>
2015-04-05 18:13:07 +0300

    Remove autogenerated files

--- rngd/doc/html/search/groups_71.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['quantisusbcontext',['QuantisUSBContext',['../group__QuantisUSBContext.html',1,'']]],
-  ['quantisusbdevice',['QuantisUSBDevice',['../group__QuantisUSBDevice.html',1,'']]]
-];
